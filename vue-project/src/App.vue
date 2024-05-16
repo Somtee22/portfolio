c3bbc8932a56b8f041c5cf661e4e05702f2b30d3
Scholastica Ngene <nnemms@gmail.com>
2024-05-16 10:05:40 +0100

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Nav from './components/Navbar.vue'
+
+</script>
+
+<template>
+  <header>
+  
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <Nav />
+  </main>
+</template>
+
---
+++ vue-project/src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <header>
+    <div class="d-flex justify-content-between">
+      <!-- <div class="img-fluid">
+            <img class=""src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGnZCvCRrBcvNzEfnp-5wvY8wjGNWWOVF-1G0hPaoy7A&s" alt="logo" />
+            </div> -->
+      <div>
+        <h1 class="mt-3">SCHOLLY</h1>
+      </div>
+      <nav class="">
+        <ul class="list justify-content-center">
+          <li class="nav-item ml-4">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacts</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <button class="rounded-lg bg-warning">Hire Me</button>
+  </header>
+
+  <div class=""  style=" background-color: pink;">
+  <div class="Hero d-flex m-auto justify-content-around" style=" height: 100vh; width: 80%;">
+    <div class="info flex flex-column align-content-center w-50 p-4">
+      <h4 class="text-white fs-4"><em>Hello, </em><span>  i am</span></h4>
+      <h2 class="">Scholastica Ngene</h2>
+      <h4 class="text-info lh-3">I am a web developer</h4>
+     <p> A passionate web developer with a penchant for crafting engaging digital experiences.
+       With a blend of creativity and technical prowess, I specialize in turning ideas into dynamic websites and applications that leave a lasting impression.
+
+Driven by a constant thirst for innovation, I thrive on exploring the latest trends and technologies in the ever-evolving world of web development. Whether it's sleek and intuitive user interfaces or robust backend systems,
+ I'm dedicated to delivering solutions that not only meet but exceed expectations.</p>
+    </div>
+  
+  <div class="img-fluid flex align-content-center mr-4 border-info" style=" width: 50%;" >
+    <img class="w-75 flex img-thumbnail" src="../assets/Scholly.jpg" alt="img" />
+  </div>
+</div>
+</div>
+</template>
+<script></script>
+
+<style setup>
+
+header {
+  display: flex;
+  justify-content: space-around;
+  width: 90%;
+}
+</style>
